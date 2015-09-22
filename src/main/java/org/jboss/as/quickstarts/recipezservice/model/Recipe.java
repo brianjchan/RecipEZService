290bae3e02daee43e999d9f48cf15cb6664636d7
Brian <brianjchan93@gmail.com>
2015-09-21 19:35:49 -0700

    '1'

--- src/main/java/org/jboss/as/quickstarts/recipezservice/model/Recipe.java
+++ src/main/java/org/jboss/as/quickstarts/recipezservice/model/Recipe.java
@@ -1,5 +1,5 @@
-//package org.jboss.as.quickstarts.recipezservice.model;
-//
+package org.jboss.as.quickstarts.recipezservice.model;
+
 //import java.io.Serializable;
 //
 //import javax.persistence.Column;
@@ -19,65 +19,65 @@
 //
 //@Entity
 //@XmlRootElement
-////@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
-//public class Recipe implements Serializable {
-//
-//  // @Id
-// //  @GeneratedValue
-//   private String id;
-//
-//  // @NotNull
-//  // @Size(min = 1, max = 25)
-//  // @Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
-//   private String name;
-//
-//  // @NotNull
-//  // @NotEmpty
-//  // @Email
-//   private String email;
-//
-////   @NotNull
-////   @Size(min = 10, max = 12)
-////   @Digits(fraction = 0, integer = 12)
-////   @Column(name = "phone_number")
-//   private String phoneNumber;
-//   
-//   public Recipe(){
-//	   id = "IDNUMBA";
-//	   name = "WHAT??";
-//	   email = "123@gmail.com";
-//	   phoneNumber= "WOAH";
-//   }
-//
-//   public String getId() {
-//      return id;
-//   }
-//
-//   public void setId(String id) {
-//      this.id = id;
-//   }
-//
-//   public String getName() {
-//      return name;
-//   }
-//
-//   public void setName(String name) {
-//      this.name = name;
-//   }
-//
-//   public String getEmail() {
-//      return email;
-//   }
-//
-//   public void setEmail(String email) {
-//      this.email = email;
-//   }
-//
-//   public String getPhoneNumber() {
-//      return phoneNumber;
-//   }
-//
-//   public void setPhoneNumber(String phoneNumber) {
-//      this.phoneNumber = phoneNumber;
-//   }
-//}
+//@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class Recipe implements Serializable {
+
+  // @Id
+ //  @GeneratedValue
+   private String id;
+
+  // @NotNull
+  // @Size(min = 1, max = 25)
+  // @Pattern(regexp = "[A-Za-z ]*", message = "must contain only letters and spaces")
+   private String name;
+
+  // @NotNull
+  // @NotEmpty
+  // @Email
+   private String email;
+
+//   @NotNull
+//   @Size(min = 10, max = 12)
+//   @Digits(fraction = 0, integer = 12)
+//   @Column(name = "phone_number")
+   private String phoneNumber;
+   
+   public Recipe(){
+	   id = "IDNUMBA";
+	   name = "WHAT??";
+	   email = "123@gmail.com";
+	   phoneNumber= "WOAH";
+   }
+
+   public String getId() {
+      return id;
+   }
+
+   public void setId(String id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public String getPhoneNumber() {
+      return phoneNumber;
+   }
+
+   public void setPhoneNumber(String phoneNumber) {
+      this.phoneNumber = phoneNumber;
+   }
+}
