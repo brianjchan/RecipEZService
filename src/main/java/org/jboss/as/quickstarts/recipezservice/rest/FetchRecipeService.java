4f3eef443ce91232c83d6e55f8c1d5d8b400b2da
Brian <brianjchan93@gmail.com>
2015-09-21 19:10:18 -0700

    '1'

--- src/main/java/org/jboss/as/quickstarts/recipezservice/rest/FetchRecipeService.java
+++ src/main/java/org/jboss/as/quickstarts/recipezservice/rest/FetchRecipeService.java
@@ -1,28 +1,28 @@
-//package org.jboss.as.quickstarts.recipezservice.rest;
-//
-//import java.util.List;
-//
-//import javax.enterprise.context.RequestScoped;
-//import javax.inject.Inject;
-//import javax.persistence.EntityManager;
-//import javax.ws.rs.GET;
-//import javax.ws.rs.Path;
-//import javax.ws.rs.PathParam;
-//import javax.ws.rs.Produces;
-//
-//import org.jboss.as.quickstarts.recipezservice.model.Recipe;
-//
-///**
-// * JAX-RS Example
-// * 
-// * This class produces a RESTful service to read the contents of the members table.
-// */
+package org.jboss.as.quickstarts.recipezservice.rest;
+
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import org.jboss.as.quickstarts.recipezservice.model.Recipe;
+
+/**
+ * JAX-RS Example
+ * 
+ * This class produces a RESTful service to read the contents of the members table.
+ */
 //@Path("/recipes")
 //@RequestScoped
-//public class FetchRecipeService {
-//   @Inject
-//   private EntityManager em;
-//
+public class FetchRecipeService {
+   @Inject
+   private EntityManager em;
+
 //   @GET
 //   @Produces("text/xml")
 //   public Recipe listAllMembers() {
@@ -44,4 +44,4 @@
 //   public Recipe lookupMemberById(@PathParam("id") long id) {
 //      return em.find(Recipe.class, id);
 //   }
-//}
+}
