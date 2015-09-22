f01ec9ce7dfedac8d5d9d29c7c1d8d3a2eb9ad0f
Brian <brianjchan93@gmail.com>
2015-09-21 17:24:15 -0700

    '1'

--- src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationTest.java
+++ src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationTest.java
@@ -8,9 +8,9 @@ import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.as.quickstarts.kitchensink.controller.MemberRegistration;
-import org.jboss.as.quickstarts.kitchensink.model.Member;
-import org.jboss.as.quickstarts.kitchensink.util.Resources;
+import org.jboss.as.quickstarts.recipezservice.controller.MemberRegistration;
+import org.jboss.as.quickstarts.recipezservice.model.Member;
+import org.jboss.as.quickstarts.recipezservice.util.Resources;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
