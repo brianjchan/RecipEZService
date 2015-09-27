183afcd8cd6c0a50fdab0c0f9578682647338c2b
Brian <brianjchan93@gmail.com>
2015-09-27 12:25:53 -0700

    Services

---
+++ src/main/java/org/jboss/as/quickstarts/recipezservice/rest/FetchUserService.java
@@ -0,0 +1,38 @@
+package org.jboss.as.quickstarts.recipezservice.rest;
+
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import org.jboss.as.quickstarts.recipezservice.model.Recipe;
+
+/**
+ * JAX-RS Example
+ * 
+ * This class produces a RESTful service to read the contents of the members table.
+ */
+@Path("/users")
+@RequestScoped
+public class FetchRecipeService {
+   @Inject
+   private EntityManager em;
+
+   @GET
+   @Produces("text/xml")
+   public Recipe listAllMembers() {
+      // Use @SupressWarnings to force IDE to ignore warnings about "genericizing" the results of
+      // this query
+      @SuppressWarnings("unchecked")
+     
+      User testUse = new User();
+      return testUse;
+   }
+
+
+}
